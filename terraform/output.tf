9704e3dee32d981facddbf0f7810ca81f7c19313
Diego Leal <souza.dleal@gmail.com>
2026-08-01 16:30:57 -0300

    feat(terraform/outputs): add control_plane_endpoint output for nlb dns

--- terraform/output.tf
+++ terraform/output.tf
@@ -7,3 +7,8 @@ output "worker_ids" {
   description = "List of instance IDs for worker nodes."
   value       = module.ec2.worker_ids
 }
+
+output "control_plane_endpoint" {
+  description = "DNS name of the NLB fronting the Kubernetes API server."
+  value       = module.balancer.nlb_dns_name
+}
